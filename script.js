abb1e582bd68d4a1d2991dfd05fc07d414fe81de
tichane <ichane.theo@gmail.com>
2025-01-29 19:03:21 +0100

    portfolio/v1.0

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
 const user_page = document.getElementById('user_info');
-const proj_page = document.getElementById('projects');
+const proj_page = document.getElementById('projects_page');
 const skill_page = document.getElementById('skills_page');
 
+const user_button = document.getElementById('user');
+const proj_button = document.getElementById('folder_projects');
+const skill_button = document.getElementById('skill');
+
 const pages = {
-    user:   user_page,
-    projects: proj_page,
-    skills: skill_page
+    user:   [user_page,user_button],
+    projects: [proj_page,proj_button],
+    skills: [skill_page,skill_button]
 };
 
 window.onload = displayPages('user')
@@ -13,9 +17,12 @@ window.onload = displayPages('user')
 function displayPages(page) {
     Object.entries(pages).forEach(([k,v]) => {
         if (k === page) {
-            v.style.display = 'flex'
+            v[0].style.display = 'flex'
+            v[1].style.border = '3px solid black'
+            v[1].style.borderRadius = '5%'
         } else {
-            v.style.display = 'none'
+            v[0].style.display = 'none'
+            v[1].style.border = 'none'
         }
     })
 }
